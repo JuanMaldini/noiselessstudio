3a7520de15d1a5459a72c4aa39bb2a1a24507375
Juan Maldini <juan.maldini@outlook.com>
2025-08-03 16:51:42 -0300

    Fix URLs in project descriptions
    Updated incorrect 'vixor3d' URLs to 'vizor3d.com' in the project descriptions for better accuracy and to ensure links work as intended.

--- src/data/projects.ts
+++ src/data/projects.ts
@@ -36,12 +36,12 @@ export const projects: Project[] = [
   {
     path: sample_02,
     title: "360 Viewer",
-    description: "http://vixor3d/Noiseless_Sample01",
+    description: "http://vizor3d.com/Noiseless_Sample01",
   },
   {
     path: sample_03,
     title: "360 Viewer",
-    description: "http://vixor3d/Noiseless_Sample01",
+    description: "http://vizor3d.com/Noiseless_Sample01",
   },
   {
     path: sample_04,
